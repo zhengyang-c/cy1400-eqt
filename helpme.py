fac50b1d8b45284961ce699d25fb72d0f3d6d86f
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2021-01-14 17:34:53 +0800

    init

---
+++ helpme.py
@@ -0,0 +1,215 @@
+import matplotlib
+import numpy as np
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import os
+
+
+def log(scnl, picks, polarity, snr, unc, pick_string, file_name):
+	# idk what to do with this
+	text_file_name = os.path.join("picks", "whole_day", "indiv_" + pick_string, "summary_" + file_name + ".txt")
+
+	headers = ["picks", "polarity", "snr", "unc"]
+	texts = [picks, polarity, snr, unc]
+	for i in range(len(headers)):
+		print("{}:{}".format(headers[i], texts[i]))
+
+	with open(text_file_name, "w" ) as f:
+		f.write("#" + "\t".join(headers)+"\n")
+		for i in range(len(picks)):			
+			f.write("{}\t{}\t{}\t{}\n".format(picks[i], polarity[i], snr[i], unc[i]))
+
+'''trace: direct numpy time series'''
+def plot_1(time, trace, file_name, save_folder): # plot only ground motion trace
+	plt.title(file_name)
+	plt.xlabel("Time (s)")
+	plt.ylabel("Ground motion (m)")
+	plt.plot(time, trace, color = 'black', linewidth=0.1)
+	plt.savefig(os.path.join(save_folder, "{}.png".format(datetime.now().strftime("%Y%m%d %H%M"))), dpi = 300)
+
+
+def plot_2(time, trace, trace2, file_name, save_folder): # plot ground motion + cf (classic) 
+	fig, axs = plt.subplots(2)
+	fig.suptitle(file_name)
+
+	axs[0].plot(time, trace, color = 'black', linewidth = 0.1)
+	#axs[0].set(xlabel='Time (s)')
+
+	axs[1].plot(time, trace2, color = 'blue', linewidth = 0.2)
+	axs[1].set(xlabel='Time (s)')
+
+	plt.savefig(os.path.join(save_folder, "{}_{}-{}.png".format(datetime.now().strftime("%Y%m%d %H%M%S"), start_time, end_time)), dpi = 300)
+
+''' depreciated '''
+def plot_3(): # plot ground motion + different cfs
+	fig, axs = plt.subplots(6, figsize=(8,10))
+	fig.tight_layout()
+	fig.suptitle(file_name)
+
+	axs[0].plot(time, trace.data, color = 'black', linewidth = 0.1)
+
+	cfs = ["classic sta lta", "recursive sta lta", "delayed sta lta", "carl sta trig", "z detect"]
+	cf_data = [cf_classic.data, cf_recursive.data, cf_delayed.data, cf_carl.data, cf_z.data]
+	#axs[0].set(xlabel='Time (s)')
+
+	for n in range(1,6):
+		axs[n].plot(time, cf_data[n - 1], color = 'blue', linewidth = 0.2)
+		axs[n].set_title(cfs[n - 1], fontsize=8)
+
+
+	plt.savefig(os.path.join(save_folder, "{}_{}-{}.png".format(datetime.now().strftime("%Y%m%d %H%M%S"), start_time, end_time)), dpi = 300)
+
+
+'''depreciated'''
+def plot_4(): # plot ground motion + classic and recursive with different exponents
+	fig, axs = plt.subplots(5,2, figsize=(14,8))
+	fig.tight_layout()
+	fig.suptitle(file_name)
+
+	axs[0,0].plot(time, trace.data, color = 'black', linewidth = 0.1)
+	axs[0,1].plot(time, trace.data, color = 'black', linewidth = 0.1)
+
+	cfs = ["classic sta lta (power 1)", "power 2", "power 3", "power 4"]
+	cf_data = [cf_classic.data, cf_classic_2.data, cf_classic_3.data, cf_classic_4.data]
+	#axs[0].set(xlabel='Time (s)')
+
+	for n in range(1,5):
+		axs[n,0].plot(time, cf_data[n - 1], color = 'blue', linewidth = 0.2)
+		axs[n,0].set_title(cfs[n - 1], fontsize=8)
+
+	cfs = ["recursive sta lta (power 1)", "power 2", "power 3", "power 4"]
+	cf_data = [cf_recursive.data, cf_recursive_2.data, cf_recursive_3.data, cf_recursive_4.data]
+
+	for n in range(1,5):
+		axs[n,1].plot(time, cf_data[n - 1], color = 'blue', linewidth = 0.2)
+		axs[n,1].set_title(cfs[n - 1], fontsize=8)
+
+	plt.savefig(os.path.join(save_folder, "classic recursive sta lta power comparison_{}_{}-{}.png".format(datetime.now().strftime("%Y%m%d %H%M%S"), start_time, end_time)), dpi = 300)
+	plt.clf()
+
+'''depreciated'''
+def plot_5(file_name): # plot ground motion + classic with different sta/lta windows 
+	fig, axs = plt.subplots(6, figsize=(8,10))
+	fig.tight_layout()
+	fig.suptitle(file_name)
+
+	axs[0].plot(time, all_cfs[0], color = 'black', linewidth = 0.1)
+	titles = ["1/10", "0.5/5", "2/20", "2/10", "0.1/5"]
+
+	for n in range(1,6):
+		axs[n].plot(time, all_cfs[n], color = 'blue', linewidth = 0.2)
+		axs[n].set_title(titles[n-1], fontsize=8)
+
+
+	plt.savefig(os.path.join(save_folder, "timewindow comparison_{}_{}-{}.png".format(datetime.now().strftime("%Y%m%d %H%M%S"), start_time, end_time)), dpi = 300)
+
+
+
+# this is to compare different picking functions
+def plot_phasepa_picks_together(file_name, time, trace, pick_string, *picks): 
+	plot_file_name = os.path.join("picks", pick_string + "_" +  file_name + ".png")
+	
+	fig, axs = plt.subplots(1 + len(picks), figsize = (8, 10))
+	fig.suptitle(file_name)
+	fig.tight_layout()
+
+
+	axs[0].plot(time, trace, color = 'black', linewidth = 0.1)
+	axs[len(picks)].set(xlabel='Time (s)')
+
+	for n, pk in enumerate(picks):
+		axs[n + 1].plot(time, trace, color = 'black', linewidth = 0.1)
+		axs[n + 1].set(title = pick_string.split("_")[n])
+		for pick in pk:
+			pick = pick - start_UTC_time - start_time
+			pick = int(pick * df)
+			axs[n + 1].plot([time[pick], time[pick]], [-1e-6, 1e-6], color = 'red', linewidth = 1, linestyle = 'dashed')
+
+	plt.savefig(plot_file_name, dpi = 300)
+	plt.clf()
+
+
+def plot_phasepa_picks(file_name, time, trace, timeshift, triggers, picks, pick_type): # plot all picks from a list picks (from phasePA, picks are in seconds from start)
+	# for each pick, output a txt file with ascii time series data, file name the filename of the thing + plot 
+
+	# the plot will have a red line drawn at the pick time 
+
+	before_trigger, after_trigger = triggers
+
+	for n, pick in enumerate(picks):
+		# plot file
+
+		pick = pick - start_UTC_time - start_time
+
+		pick = int(pick * df)
+
+		plot_file_name = os.path.join("picks", "indiv_" + pick_type, "phasepa_" + pick_type + "_" + file_name + "_" + str(n).zfill(3) + ".png")
+		ascii_file_name = os.path.join("picks", "indiv_" + pick_type, "phasepa_" + pick_type + "_" + file_name + "_" + str(n).zfill(3) + ".txt")
+
+		plt.title("Pick {}:{}".format(str(n).zfill(3), file_name))
+		plt.plot(time[pick - before_trigger : pick + after_trigger], trace[pick - before_trigger : pick + after_trigger], color = 'black', linewidth = 0.2)
+		plt.plot([time[pick], time[pick]], [-1e-6, 1e-6], color = 'red', linewidth = '1', linestyle = 'dashed')
+
+		plt.savefig(plot_file_name)
+		plt.clf()
+
+		with open(ascii_file_name, 'w') as f:
+			for i in range(len(trace[pick - before_trigger : pick + after_trigger])):
+				f.write("{}\t{}\n".format((time[pick - before_trigger : pick + after_trigger] * 0.0080)[i], trace[pick - before_trigger : pick + after_trigger][i]))
+
+	# also plot all the picks on the same plot 
+
+	plot_file_name = os.path.join("picks", "all_" + pick_type + "_" +  file_name + ".png")
+	ascii_file_name = os.path.join("picks", "all_" + pick_type + "_" + file_name + ".txt")
+	plt.title(file_name)	
+	plt.xlabel("Time (s)")
+	plt.ylabel("Ground motion (m)")
+	plt.plot(time, trace, color = 'black', linewidth=0.1)
+	for pick in picks:
+		pick = pick - timeshift
+		pick = int(pick * df)
+		plt.plot([time[pick], time[pick]], [-1e-6, 1e-6], color = 'red', linewidth = 1, linestyle = 'dashed')
+
+	plt.savefig(plot_file_name, dpi = 300)
+	plt.clf()
+
+def plot_obspy_picks_together(file_name, time, trace, picks, pick_string):
+
+	plt.xlabel("Time (s)")
+	plt.ylabel("Ground motion (m)")
+
+	plt.plot(time, trace, color = 'black', linewidth = 0.1)
+	plot_file_name = os.path.join("picks", "obspy_" + pick_string + "_" + file_name + ".png")
+	for (pick,_) in picks:
+		# plot file
+		plt.plot([time[pick], time[pick]], [-1e-6, 1e-6], color = 'red', linewidth = '1', linestyle = 'dashed')
+
+	plt.savefig(plot_file_name)
+	plt.clf()
+
+
+def plot_obspy_picks(file_name, time, trace, triggers, start_UTC_time, picks, pick_type): # plot all picks from a list picks (from the trigger_onset function)
+	# for each pick, output a txt file with ascii time series data, file name the filename of the thing + plot 
+
+	# the plot will have a red line drawn at the pick time 
+	(before_trigger, after_trigger) = triggers
+
+	for n, (pick,_) in enumerate(picks):
+		# plot file
+		event_time = start_UTC_time + pick
+
+		event_time_string = event_time.strftime("%H_%M_%S")
+		plot_file_name = os.path.join("picks", "indiv_obspy" , pick_type + "_" + file_name + "_" + event_time_string + ".png")
+		ascii_file_name = os.path.join("picks", "indiv_obspy", pick_type + " _" + file_name + "_" + event_time_string + ".txt")
+
+		plt.title("Pick {}:{}".format(str(n).zfill(3), file_name))
+		plt.plot(time[pick - before_trigger : pick + after_trigger], trace[pick - before_trigger : pick + after_trigger], color = 'black', linewidth = 0.2)
+		plt.plot([time[pick], time[pick]], [-1e-6, 1e-6], color = 'red', linewidth = '1', linestyle = 'dashed')
+
+		plt.savefig(plot_file_name, dpi = 300)
+		plt.clf()
+
+		with open(ascii_file_name, 'w') as f:
+			for i in range(len(trace[pick - before_trigger : pick + after_trigger])):
+				f.write("{}\t{}\n".format((time[pick - before_trigger : pick + after_trigger] * 0.0080)[i], trace[pick - before_trigger : pick + after_trigger][i]))
+
---
+++ use_eqt.py
@@ -0,0 +1,214 @@
+from EQTransformer.core.predictor import predictor
+from EQTransformer.utils.hdf5_maker import preprocessor
+import obspy
+from obspy import read
+import numpy as np
+import os
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+from datetime import datetime
+from helpme import *
+import sys
+import re
+import json
+import datetime
+import pandas as pd
+import glob
+import copy
+
+coordinates_doc = "station_info.dat"
+station_json_output = 'station_list.json'
+query_station_day = ["TA01_2020_085"] # some day autogenerate this lol
+mseed_parent_folder_name = "EOS_MSEED"
+data_parent_folder_name = "EOS_SAC" # what folder structure am i uh using 
+mseed_hdfs = mseed_parent_folder_name + "_processed_hdfs"
+eqt_model_path = 'EQTransformer/ModelsAndSampleData/EqT_model.h5'
+detection_folder_name = "detections 20210113 1413"
+
+# generate stations.json for a single station
+
+station_list = {}
+for thing in query_station_day:
+	[_station, _year, _day] = thing.split("_")
+	station_list[_station] = {"network": "AC", "channels":["EHZ", "EHE", "EHN"]}
+
+# generate station_list.json for multiple stations / handle a batch of files 
+
+with open(coordinates_doc, "r") as f:
+	coordinates = f.read().split("\n")
+	
+	coordinates = [y.strip() for x in coordinates if len(x) > 0 for y in x.strip().split("\t") if len(y) > 0 ]
+
+	# get all the stations and write the relevant details
+	# all use 3 channels
+	# elevations just put like 100? lmao
+
+for station in station_list:
+	#print(station)
+	i = coordinates.index(station)
+
+	station_list[station]["coords"] = [100, float(coordinates[i + 1]), float(coordinates[i+2])]
+
+with open(station_json_output, 'w') as f:
+	json.dump(station_list, f)
+
+# convert sac files to mseed for use in EQT
+
+
+
+# EOS_MSEED
+#	TA01
+# 		sac file
+
+folders = [x[0] for x in os.walk(data_parent_folder_name)]
+
+# if the last few letters follow a specific regex pattern, then use that folder
+
+folders = list(filter(lambda x: re.match(r"\D{2,3}\d{2,3}", x.split("/")[-1]), folders))
+
+# presumably everything will be first converted from MSEED to SAC format (preprocessed)
+# then back to MSEED for use in EqT
+# and then plotting using the SAC file 
+
+# for each folder, build a tuple of (EHE, EHZ, EHN) for every single day
+
+all_files = []
+
+for folder in folders:
+	files = {}
+
+	for _file in os.listdir(folder):
+		net = _file.split(".")[0]
+		sta = _file.split(".")[1]
+		_ = _file.split(".")[2] #idk dude
+		cha = _file.split(".")[3]
+		_ = _file.split(".")[4]
+		year_day = _file.split(".")[5] + "_" + _file.split(".")[6]
+		
+		if year_day not in files:
+			files[year_day] = [_file]
+		elif year_day in files:
+			files[year_day].append(_file)
+
+	all_files.append((sta, files))
+		
+
+#print(all_files)
+# convert them to MSEED format using OBSPY
+for _station, _all_days in all_files:
+	for year_day in _all_days:
+		for _file in _all_days[year_day]:
+
+			# month
+			_month = datetime.datetime.strptime(_file.split(".")[6], "%j").strftime("%m")
+
+			_mseed_file_name = "AC__{}__{}__{}__{}.mseed".format(_file.split(".")[5], _month, _station, _file.split(".")[3]) # 3: channel
+			# AC__2020__03__TA01__EHZ i think??
+			# if mseed file is written already, don't bother 
+			# TODO: add a flag to force writing (?) no point tbh
+			if os.path.isfile(os.path.join(mseed_parent_folder_name, _station, _mseed_file_name)):
+				continue
+
+			# check if output file exists
+			_st = read(os.path.join(data_parent_folder_name, _station, _file))
+			
+			# make output folder if it's not there 
+			if not os.path.exists(os.path.join(mseed_parent_folder_name, _station)):
+				os.makedirs(os.path.join(mseed_parent_folder_name, _station))
+
+			_st.write(os.path.join(mseed_parent_folder_name, _station, _mseed_file_name), format = 'MSEED')
+
+
+if not os.path.exists(mseed_hdfs):
+	preprocessor(mseed_dir=mseed_parent_folder_name, stations_json= station_json_output, overlap=0.3, n_processor=4)
+
+# this is quite sketchy hmm
+if not os.path.exists(detection_folder_name):
+	predictor(input_dir = mseed_hdfs, input_model = eqt_model_path, output_dir=detection_folder_name, detection_threshold=0.3, P_threshold=0.1, S_threshold=0.1, number_of_plots=50, plot_mode='time')
+
+# read CSV to cut SAC files into smaller folders and stuff
+
+# i'm unsure what will happen with batch processing
+
+for csv_file in glob.glob("{}/*/*.csv".format(detection_folder_name)):
+	df = pd.read_csv(csv_file)
+
+	sta = csv_file.split("/")[1].split("_")[0]
+
+	csv_dir = "/".join(csv_file.split("/")[:-1])
+
+	save_dir = "sac_picks"
+
+	if not os.path.exists(os.path.join(csv_dir, save_dir)):
+		os.makedirs(os.path.join(csv_dir, save_dir))
+		
+	pick_date_times = []
+
+	for p_arrival in df['p_arrival_time']:
+		pick_date_times.append(datetime.datetime.strptime(p_arrival.split(".")[0], "%Y-%m-%d %H:%M:%S"))
+
+	#for p_arrival in pick_date_times:
+
+	for _sta, _sta_details in all_files:
+		if _sta == sta:
+			sta_details = copy.deepcopy(_sta_details)
+			break
+
+	assert sta_details
+
+	for c, p_arrival in enumerate(pick_date_times):
+		# get year and julian day
+		prev_year_day = ""
+		_year = p_arrival.strftime("%Y")
+		_day = p_arrival.strftime("%j")
+		pick_year_day =  _year + "_" + _day
+
+		print(sta_details[pick_year_day])
+
+		if c == 0:
+			st = read(os.path.join(data_parent_folder_name, sta,"*{}.{}.*".format(_year, _day))) 
+
+		elif not prev_year_day == pick_year_day: # different year_day, so reload new
+			st.clear()
+			st = read(os.path.join(data_parent_folder_name, sta,"*{}.{}.*".format(_year, _day))) 
+
+		# else, it's already loaded, can start trimming
+		_st = st.copy()
+
+		start_UTC_time = _st[0].stats.starttime
+		delta_t = obspy.UTCDateTime(p_arrival) - start_UTC_time
+		_st.trim(start_UTC_time + delta_t - 30, start_UTC_time + delta_t + 120)
+
+
+		# write SAC file 
+		for tr in _st:
+			if not os.path.exists(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, p_arrival.strftime("%H%M%S"), tr.stats.channel))):
+				tr.stats.stla, tr.stats.stlo = station_list[sta]["coords"][1], station_list[sta]["coords"][2], 
+				#print(tr.stats.stla)
+				#print(tr.stats.stlo)
+				tr.write(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, tr.stats.channel, p_arrival.strftime("%H%M%S"))), format = "SAC")
+
+
+		# plot 
+
+		# trim a bit more then plot ?
+		_st.trim(start_UTC_time + delta_t - 5, start_UTC_time + delta_t + 10)
+		png_file = "{}.{}.{}.{}.png".format(sta, _year, _day, p_arrival.strftime("%H%M%S"))
+		if not os.path.exists(os.path.join(csv_dir, save_dir, png_file)):
+			_st.plot(outfile = os.path.join(csv_dir, save_dir, png_file), size = (800, 600))
+
+
+		prev_year_day = pick_year_day
+
+# find the corresponding SAC file
+
+# load all three components
+
+# for each component, trim to:
+# 30 s before P and wave and 2 minutes after P wave
+
+# write the SAC file into a new folder
+
+# and presumably also plot in mpl or something 
+
