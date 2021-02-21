55885eb008243b60ec4179c9f223df2bc4708723
Zheng Yang <ZCHOONG001@e.ntu.edu.sg>
2021-02-21 22:01:16 +0800

    fix

--- rename_channel.sh
+++ rename_channel.sh
@@ -7,5 +7,5 @@ mapfile -t sac_files < <( ls $1/sac_picks | grep .SAC | sed -n '0~3p')
 for f in ${sac_files[@]} # f is a file name to read
 do 
 	echo "$1/sac_picks/$f" # the file to read from sac
-	awk -F "." '{print $1.$2.$3.$5.$4.SAC}' "$1/sac_picks/$f" 
+	echo "$1/sac_picks/$f" | awk -F "." '{print $1"."$2"."$3"."$5"."$4".SAC"}' 
 done
