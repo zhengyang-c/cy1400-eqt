e4508d7c20df65745bc8bf1a1b652ed71327622e
Zheng Yang <ZCHOONG001@e.ntu.edu.sg>
2021-02-24 17:08:24 +0800

    i hate this

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ STA=TA19
 
 #python sac_to_mseed.py $SAC_FOLDER
 #python mseed_to_h5.py "mseed_${SAC_FOLDER}" $STATION_DATA $STA
-python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
-python plot_eqt.py $SAC_FOLDER $OUTPUT_FOLDER
+#python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
+#python plot_eqt.py $SAC_FOLDER $OUTPUT_FOLDER
 python header_writer.py "${OUTPUT_FOLDER}/${STA}_outputs/X_prediction_results.csv" "${OUTPUT_FOLDER}/${STA}_outputs/header.txt" $STATION_DATA
 
 ./writerino.sh $OUTPUT_FOLDER "${OUTPUT_FOLDER}/${STA}_outputs/X_prediction_results.csv"
