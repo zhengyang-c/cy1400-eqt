a742b0b9125b971e1df88c07a382611e30b8dab8
Zheng Yang <ZCHOONG001@e.ntu.edu.sg>
2021-02-24 16:42:54 +0800

    yes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ MODEL=/home/zchoong001/cy1400/cy1400-eqt/EQTransformer/ModelsAndSampleData/EqT_m
 STATION_DATA=station_info.dat
 OUTPUT_FOLDER="detections/single_day_test"
 
-echo python sac_to_mseed.py $SAC_FOLDER
-echo python mseed_to_h5.py "mseed_${SAC_FOLDER}" $STATION_DATA TA19
-echo python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
-echo python plot_eqt.py $SAC_FOLDER $OUTPUT_FOLDER
+#python sac_to_mseed.py $SAC_FOLDER
+#python mseed_to_h5.py "mseed_${SAC_FOLDER}" $STATION_DATA TA19
+python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
+python plot_eqt.py $SAC_FOLDER $OUTPUT_FOLDER
