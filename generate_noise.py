471d228247b7adb5e867d52fe1815879734d4d17
Zheng Yang <ZCHOONG001@e.ntu.edu.sg>
2021-03-13 16:14:15 +0800

    please end me

--- generate_noise.py
+++ generate_noise.py
@@ -164,7 +164,7 @@ def collate_timestamps():
 			for j in range(math.floor(n_cuts)):
 				new_start = _i[1] + datetime.timedelta(seconds = overlap * 60) * j
 				new_end = new_start + datetime.timedelta(seconds = 60)
-			new_cut_noise_ts.append((new_start, new_end))
+				new_cut_noise_ts.append((new_start, new_end))
 
 		print(new_cut_noise_ts)		
 		
