89fa598268e4a28e90ee5bee238bbe6545d1a2bf
Zheng Yang <ZCHOONG001@e.ntu.edu.sg>
2021-03-14 21:00:56 +0800

    predict using new aceh model

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 SAC_FOLDER=single_day_test
 DEFAULT_MODEL=/home/zchoong001/cy1400/cy1400-eqt/EQTransformer/ModelsAndSampleData/EqT_model.h5
-MODEL=/home/zchoong001/cy1400/cy1400-eqt/models/30000_70000_STEAD_LR1e-3_outputs/final_model.h5
-MODEL2=/home/zchoong001/cy1400/cy1400-eqt/models/3000_7000_STEAD_LR1e-3_outputs/final_model.h5
+MODEL=/home/zchoong001/cy1400/cy1400-eqt/models/14mar_aceh_with_noise_LR1e-6_outputs/final_model.h5
+
 STATION_DATA=station_info.dat
-OUTPUT_FOLDER="detections/13mar_checknoisesamples"
+OUTPUT_FOLDER="detections/14mar_acehtrainingdata"
 OUTPUT_FOLDER2="detections/1e4model_LR1e-3_TA19.085"
 STA=TA19
 
 #python sac_to_mseed.py $SAC_FOLDER
 #python mseed_to_h5.py "mseed_${SAC_FOLDER}" $STATION_DATA $STA
-#python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
-python run_eqt.py /home/zchoong001/cy1400/cy1400-eqt/training_files/aceh_noise_13mar_wholeday $DEFAULT_MODEL $OUTPUT_FOLDER
+python run_eqt.py "mseed_${SAC_FOLDER}_processed_hdfs" $MODEL $OUTPUT_FOLDER
+#python run_eqt.py /home/zchoong001/cy1400/cy1400-eqt/training_files/aceh_noise_13mar_wholeday $DEFAULT_MODEL $OUTPUT_FOLDER
 
 python plot_eqt.py $SAC_FOLDER $STA $OUTPUT_FOLDER
 
