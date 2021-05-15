4049846386006a5eb45d21eedf77f7b68b39e7f7
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-05-15 14:28:37 +0800

    cmd line arguments for trainer

--- use_train.py
+++ use_train.py
@@ -1,11 +1,18 @@
 from own_trainer import trainer
 import argparse
 
-def main(output, i):
-	print(i,'1e4samples_LR1e-{}'.format(i), 10**(-i))
-	trainer(input_hdf5='/home/zchoong001/cy1400/cy1400-eqt/training_files/14mar_aceh_with_noise.hdf5',
-		input_csv='/home/zchoong001/cy1400/cy1400-eqt/training_files/14mar_aceh_with_noise.csv',
-		output_name='{}_LR1e-{}'.format(output, i),      
+
+# output root
+# output name
+# LR
+# 
+
+
+
+def main(input_root, lr, output_folder):
+	trainer(input_hdf5=input_root + ".hdf5",
+		input_csv=input_root + '.csv',
+		output_name='{}_LR1e-{}'.format(output_folder, lr),      
 		# i suspect the output_name cannot have / or it will confuse the saving     
 		cnn_blocks=5,
 		lstm_blocks=2,
@@ -30,9 +37,11 @@ def main(output, i):
 		)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('i', type = int)
-parser.add_argument('output', type = str)
+parser.add_argument('input_root', type = str, help = "file root to .hdf5 and .csv pair")
+parser.add_argument('lr', type = int, help = "learning rate exponent")
+parser.add_argument('output_folder', type = str, help = "name of the folder in the same directory to save the model to")
 
 args = parser.parse_args()
 
-main(args.output, args.i)
+
+main(args.input_root, args.lr, args.output_folder)
