e505dac9ffba77a57671026ea0cb2b0bd391473d
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-09 16:09:21 +0800

    test multi-node

---
+++ node_distributor.py
@@ -0,0 +1,19 @@
+# goal: use gekko arrays to distribute tasks
+
+
+
+
+import argparse
+import pandas as pd
+import os
+from pathlib import Path
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-id")
+
+args = parser.parse_args()
+
+print(args.id)
