4de34bb2e4fbdce5eced8ec8ae6a84ae665cc6bb
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-09 20:15:50 +0800

    longer pbs

--- node_distributor.py
+++ node_distributor.py
@@ -1,9 +1,15 @@
 # goal: use gekko arrays to distribute tasks
 
+# prepare csv files so upon receiving an ID each script can do a lookup and get the files they need
+
 import argparse
 import pandas as pd
 import os
 from pathlib import Path
+
+# load station selector file
+
+
 
 
 
