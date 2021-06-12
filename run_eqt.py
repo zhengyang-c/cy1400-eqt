5953b3f710d843fd49bda1e736ebeab458f6ec77
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-12 23:33:58 +0800

    add start option to run

--- run_eqt.py
+++ run_eqt.py
@@ -5,7 +5,7 @@ import datetime
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 
-def run(hdf_folder, model_path, output_folder, multi = 1):
+def run(hdf_folder, model_path, output_folder, multi = 1, start = 1):
 	'''
 	hdf_folder is ${mseed_folder}_processed_hdfs
 	# '''
@@ -35,7 +35,7 @@ def run(hdf_folder, model_path, output_folder, multi = 1):
 	# 		p.map(wrapper, arglist) 
 	# else:
 	
-	for c in range(multi):
+	for c in range(start, multi + 1):
 		_output = os.path.join(output_folder, "multi_{:02d}".format(c))
 		predictor(input_dir = hdf_folder, input_model = model_path, output_dir=_output, detection_threshold=0.3, P_threshold=0.1, S_threshold=0.1, plot_mode='time', output_probabilities = False, number_of_cpus = n_cpus, number_of_plots = 0)
 
@@ -45,9 +45,10 @@ if __name__ == "__main__":
 	parser.add_argument('hdf_folder')
 	parser.add_argument('model_path')
 	parser.add_argument('output_folder')
+	parser.add_argument('-s', '--start', type = int, default = 1, help = "start number")
 	parser.add_argument('-m', '--multirun', type = int, default = 1, help = "how many repeats")
 	#parser.add_argument('-n', '--n_cpus', type = int, default = 1)
-	parser.add_argument('-t', '--time', type = str, help = "file path to append to to")
+	parser.add_argument('-t', '--time', type = str, help = "file path to append to")
 
 	#parser.add_argument('json_output', help = "this is an intermediate file needed by EqT")
 
@@ -56,7 +57,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	start_time = datetime.datetime.now()
-	run(args.hdf_folder, args.model_path, args.output_folder, args.multirun)
+	run(args.hdf_folder, args.model_path, args.output_folder, args.multirun, start = args.start)
 	
 
 	end_time = datetime.datetime.now()
