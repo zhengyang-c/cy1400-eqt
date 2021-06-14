f89d20fadf426c41e4fae91eea6c63b702601afc
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-14 11:17:21 +0800

    test recompute snr

--- recompute_snr.py
+++ recompute_snr.py
@@ -71,7 +71,7 @@ def recompute_from_sac_source(sac_select, detection_csv, output_csv):
 
 			# first row, load 
 
-			_df = (sac_df[(sac_df.station == sta) && (sac_df.year == year) && (sac_df.jday == jday)])
+			_df = (sac_df[(sac_df.station == sta) & (sac_df.year == year) & (sac_df.jday == jday)])
 			_df.reset_index(inplace = True)
 
 			print(_df.at[0, "filepath"])
@@ -81,7 +81,7 @@ def recompute_from_sac_source(sac_select, detection_csv, output_csv):
 			if year_day == prev_year_day:
 				pass
 			else:
-				_df = (sac_df[(sac_df.station == sta) && (sac_df.year == year) && (sac_df.jday == jday)])
+				_df = (sac_df[(sac_df.station == sta) & (sac_df.year == year) & (sac_df.jday == jday)])
 				_df.reset_index(inplace = True)
 
 				print(_df.at[0, "filepath"])
