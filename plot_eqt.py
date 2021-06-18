6379367da17a100748ab0e071981b8eaef43c2ba
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-18 23:20:44 +0800

    yes

--- plot_eqt.py
+++ plot_eqt.py
@@ -6,6 +6,8 @@ import glob
 import os
 import datetime
 
+import matplotlib.pyplot as plt
+
 from pathlib import Path
 
 
@@ -15,79 +17,92 @@ def str_to_datetime(x):
 	except:
 		return datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
 
-def plot(data_parent_folder_name, sta, detection_folder_name):
+def plot(data_parent_folder_name, csv_file):
 
-	for csv_file in [str(path) for path in Path(detection_folder_name).rglob("*.csv")]:
+	print(csv_file)
+	df = pd.read_csv(csv_file)		
 
-		print(csv_file)
-		df = pd.read_csv(csv_file)		
+	csv_dir = "/".join(csv_file.split("/")[:-1])
 
-		csv_dir = "/".join(csv_file.split("/")[:-1])
+	save_dir = "sac_picks"
 
-		save_dir = "sac_picks"
+	if not os.path.exists(os.path.join(csv_dir, save_dir)):
+		os.makedirs(os.path.join(csv_dir, save_dir))
+		
+	pick_date_times = []
 
-		if not os.path.exists(os.path.join(csv_dir, save_dir)):
-			os.makedirs(os.path.join(csv_dir, save_dir))
-			
-		pick_date_times = []
+	# not actually the p_arrival but i switched gears while writing this so
 
-		# not actually the p_arrival but i switched gears while writing this so
+	for p_arrival in df['event_start_time']:
+		pick_date_times.append(str_to_datetime(p_arrival))
 
-		for p_arrival in df['event_start_time']:
-			pick_date_times.append(str_to_datetime(p_arrival))
+	df['event_start_time'] = pd.to_datetime(df['event_start_time'])
 
-		#for p_arrival in pick_date_times:
-
-
-		for c, p_arrival in enumerate(pick_date_times):
-			# get year and julian day
-			prev_year_day = ""
-			_year = p_arrival.strftime("%Y")
-			_day = p_arrival.strftime("%j")
-			pick_year_day =  _year + "_" + _day
-
-			if c == 0:
-				st = read(os.path.join(data_parent_folder_name, sta,"*{}.{}.*".format(_year, _day))) 
-
-				# bp filter and demean and resample
+	#for p_arrival in pick_date_times:
 
 
-
-			elif not prev_year_day == pick_year_day: # different year_day, so reload new
-				st.clear()
-				st = read(os.path.join(data_parent_folder_name, sta,"*{}.{}.*".format(_year, _day))) 
-
-				# bp filter and demean and resample
-
-			# else, it's already loaded, can start trimming
-			_st = st.copy()
-
-			start_UTC_time = _st[0].stats.starttime
-			delta_t = obspy.UTCDateTime(p_arrival) - start_UTC_time
-			_st.trim(start_UTC_time + delta_t - 30, start_UTC_time + delta_t + 120)
+	for index, row in df.iterrows():
+		# get year and julian day
+		prev_year_day = ""
+		#_year = p_arrival.strftime("%Y")
+		#_day = p_arrival.strftime("%j")
+		#pick_year_day =  _year + "_" + _day
 
 
-			# write SAC file 
-			for tr in _st:
-				if not os.path.exists(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, p_arrival.strftime("%H%M%S"), tr.stats.channel))):
+		sta = row.station.strip()
+		event_dt = row.event_start_time
 
-					tr.write(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, p_arrival.strftime("%H%M%S"),  tr.stats.channel),), format = "SAC")
+		year = (datetime.datetime.strftime(event_dt, "%Y"))
+		jday = (datetime.datetime.strftime(event_dt, "%j"))
 
-			# trim a bit more then plot ?
-			_st.trim(start_UTC_time + delta_t - 5, start_UTC_time + delta_t + 10)
-			png_file = "{}.{}.{}.{}.png".format(sta, _year, _day, p_arrival.strftime("%H%M%S"))
-			if not os.path.exists(os.path.join(csv_dir, save_dir, png_file)):
-				_st.plot(outfile = os.path.join(csv_dir, save_dir, png_file), size = (800, 600))
+		pick_year_day = year + "."+ jday # need string representation
+
+		year, jday = int(year), int(jday) # the julian is saved as integer so need to convert (085 vs 85)
+
+		if c == 0:
+			st = read(os.path.join(data_parent_folder_name, sta,"*{}*".format(pick_year_day))) 
+
+			# bp filter and demean and resample
 
 
-			prev_year_day = pick_year_day
+
+		elif not prev_year_day == pick_year_day: # different year_day, so reload new
+			st.clear()
+			st = read(os.path.join(data_parent_folder_name, sta,"*{}*".format(pick_year_day))) 
+
+			# bp filter and demean and resample
+
+		# else, it's already loaded, can start trimming
+		_st = st.copy()
+
+		start_UTC_time = _st[0].stats.starttime
+		delta_t = obspy.UTCDateTime(p_arrival) - start_UTC_time
+		_st.trim(start_UTC_time + delta_t - 30, start_UTC_time + delta_t + 120)
+
+		# can i just use mpl plotting?
+
+
+		# write SAC file 
+		for tr in _st:
+			if not os.path.exists(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, p_arrival.strftime("%H%M%S"), tr.stats.channel))):
+
+				tr.write(os.path.join(csv_dir, save_dir, "{}.{}.{}.{}.{}.SAC".format(sta, _year, _day, p_arrival.strftime("%H%M%S"),  tr.stats.channel),), format = "SAC")
+
+		# trim a bit more then plot ?
+		_st.trim(start_UTC_time + delta_t - 5, start_UTC_time + delta_t + 10)
+		png_file = "{}.{}.{}.{}.png".format(sta, _year, _day, p_arrival.strftime("%H%M%S"))
+		if not os.path.exists(os.path.join(csv_dir, save_dir, png_file)):
+			_st.plot(outfile = os.path.join(csv_dir, save_dir, png_file), size = (800, 600))
+
+
+		prev_year_day = pick_year_day
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('sac_folder')
-	parser.add_argument('sta')
+	#parser.add_argument('sta')
 	parser.add_argument('input_folder')
 	parser.add_argument('-t', '--time', type = str, help = "file path to append to to")
 
@@ -97,7 +112,7 @@ if __name__ == "__main__":
 
 	start_time = datetime.datetime.now()
 
-	plot(args.sac_folder, args.sta, args.input_folder)
+	plot(args.sac_folder, args.input_folder)
 
 	end_time = datetime.datetime.now()
 
