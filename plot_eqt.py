fbe57354fa1f761e0ae9840f4d8f63de5d9ef06e
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-19 21:36:37 +0800

    Test plot

--- plot_eqt.py
+++ plot_eqt.py
@@ -94,7 +94,7 @@ def sac_plotter(sac_csv, csv_file):
 
 	# call subprocess
 
-	os.chmod(output_bash, 0o775)			
+	os.chmod(os.path.join(csv_dir, "cut_and_plot.sh"), 0o775)			
 
 def plot(sac_csv, csv_file):
 
