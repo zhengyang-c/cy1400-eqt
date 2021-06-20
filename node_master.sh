0c4af8fc1b56de4d2a0fc14b2fe03eb1a2c2cb37
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-20 15:32:21 +0800

    test header writing

--- node_master.sh
+++ node_master.sh
@@ -1,3 +1,3 @@
 # always use absolute paths if possible
 
-python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/random10.txt -o /home/zchoong001/cy1400/cy1400-eqt/node_encode/10jun_random10.csv -job 10jun_random10_150-151 -s 2020_150 -e 2020_151 -n_nodes 10 -pbs -hdf5_parent /home/zchoong001/cy1400/cy1400-eqt/prediction_files/9jun_10random_150-151 -sac_select /home/zchoong001/cy1400/cy1400-eqt/station_time/9jun_10station.csv -plot_eqt
+python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/random10.txt -o /home/zchoong001/cy1400/cy1400-eqt/node_encode/10jun_random10.csv -job 10jun_random10_150-151 -s 2020_150 -e 2020_151 -n_nodes 10 -pbs -hdf5_parent /home/zchoong001/cy1400/cy1400-eqt/prediction_files/9jun_10random_150-151 -sac_select /home/zchoong001/cy1400/cy1400-eqt/station_time/9jun_10station.csv -write_headers
