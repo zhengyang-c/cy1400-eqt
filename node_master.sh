310bd056444d9dd8fe353cdd7df1fc45e51ac18b
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-23 09:50:22 +0800

    test again

--- node_master.sh
+++ node_master.sh
@@ -1,5 +1,5 @@
 # always use absolute paths if possible
 
-python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/random10.txt -o /home/zchoong001/cy1400/cy1400-eqt/node_encode/10jun_random10.csv -job 10jun_random10_150-180 -s 2020.150 -e 2020.180 -n_nodes 10 -pbs -sac_select /home/zchoong001/cy1400/cy1400-eqt/station_time/20jun_random10_1month.csv -plot_eqt -write_headers
+python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/random10.txt -o /home/zchoong001/cy1400/cy1400-eqt/node_encode/10jun_random10.csv -job 23jun_random10_150-180 -s 2020.150 -e 2020.180 -n_nodes 10 -pbs -sac_select /home/zchoong001/cy1400/cy1400-eqt/station_time/20jun_random10_1month.csv -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers
 
 # -hdf5_parent /home/zchoong001/cy1400/cy1400-eqt/prediction_files/9jun_10random_150-151
