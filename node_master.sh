1cf37889380ba8ffde1072461a5c5ae69fba04d7
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-06-30 16:27:45 +0800

    test ge02 ge03 jan

--- node_master.sh
+++ node_master.sh
@@ -22,7 +22,7 @@ python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/30
 
 python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/30jun_rerun_GE0203.txt -job 30jun_rerun_GE0203_Feb -s 2020.032 -e 2020.060 -pbs -make_sac_csv -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers -no_execute
 
-python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/30jun_rerun_GE0203.txt -job 30jun_rerun_GE0203_Mar -s 2020.061 -e 2020.031 -pbs -make_sac_csv -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers -no_execute
+python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/30jun_rerun_GE0203.txt -job 30jun_rerun_GE0203_Mar -s 2020.061 -e 2020.091 -pbs -make_sac_csv -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers -no_execute
 
 #python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/GE-GM.txt -job 24jun_GE-GM_Jan-Mar -s 2020.001 -e 2020.091 -pbs -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers -no_execute
 #python multi_station.py -encode -i /home/zchoong001/cy1400/cy1400-eqt/station/GE-GM.txt -job 24jun_GE-GM_Apr-May -s 2020.092 -e 2020.183 -pbs -write_hdf5 -run_eqt -merge_csv -recompute_snr -filter_csv -plot_eqt -write_headers -no_execute
