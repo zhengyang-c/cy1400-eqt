57254cdc2b8e950a1481464d5d4a4de0e1b93235
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-07-07 16:33:39 +0800

    Check completion

--- check_completion.py
+++ check_completion.py
@@ -19,11 +19,31 @@ for job_name in job_list:
 df = pd.concat(df_list)
 
 print(df)
-
 # hdf5 dir:
 
 # look for the node encode files
 
+for index, row in df.iterrows():
+
+	# check hdf5 folder for sta.csv and sta.hdf5
+
+	flags = {
+	"sac_csv": os.path.exists(os.path.join(row.hdf5_folder, sta + ".csv")),
+	"sac_hdf5": os.path.exists(os.path.join(row.hdf5_folder, sta + ".hdf5")),
+	"n_merged": len([str(x) for x in Path(row.prediction_output_folder).glob("*_merged")])
+	"merge_filtered": os.path.exists(os.path.join(row.merge_output_folder, "merge_filtered.csv")),
+	"merge_filtered_snr": os.path.exists(os.path.join(row.merge_output_folder, "merge_filtered_snr.csv")),
+	"merge_filtered_snr_customfilter": os.path.exists(os.path.join(row.merge_output_folder, "merge_filtered_snr_customfilter.csv")),
+	"n_lines": int(subprocess.check_output(["wc", "-l", os.path.join(row.merge_output_folder, "merge_filtered_snr_customfilter.csv")]).decode("utf8").split()[0]) - 1,
+	"sac_pick_files":len(os.listdir(os.path.join(row.merge_output_folder, "sac_picks")))
+	}
+
+	print(flags)
+
+	if index > 2:
+		break
+
+	# 
 
 
 # check all hdf5 files and csv files generated
