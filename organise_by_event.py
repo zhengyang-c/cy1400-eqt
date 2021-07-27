c34fa1303a7ce1c8b859467d509274f73bc69d47
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-07-28 01:17:59 +0800

    try with all

--- organise_by_event.py
+++ organise_by_event.py
@@ -50,9 +50,9 @@ def main():
 	#print(event_df["timestamp"])
 	#event_df["timestamp"] = pd.to_datetime(event_df["timestamp"])
 
-	# for index, row in event_df.iterrows():
-	# 	searcher(row.ID, df, event_df, phase_dict, dryrun = True)
-	searcher(5, df, event_df, phase_dict, dryrun = True)
+	for index, row in event_df.iterrows():
+	 	searcher(int(row.ID), df, event_df, phase_dict)
+	#searcher(5, df, event_df, phase_dict, dryrun = True)
 	#header_writer(5, df, event_df, phase_dict)
 
 	# i think the P and S times in the phase_dict are not reliable so just use the new origin
