349ac50a415cba25bf9f26bb63d81a587d43cb02
Choong Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-08-09 16:12:47 +0800

    Typo model_path

--- multi_station.py
+++ multi_station.py
@@ -411,7 +411,7 @@ def encode_multirun(
 
 	else:
 		if not model_path:
-			model_path"/home/zchoong001/cy1400/cy1400-eqt/EQTransformer/ModelsAndSampleData/EqT_model.h5"
+			model_path = "/home/zchoong001/cy1400/cy1400-eqt/EQTransformer/ModelsAndSampleData/EqT_model.h5"
 		station_json = "/home/zchoong001/cy1400/cy1400-eqt/station/json/all_stations.json"
 
 		if hdf5_parent == "":
