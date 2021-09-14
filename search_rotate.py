Fix trying to rotate for stations for which no calculation was made

search_rotate.py
@@ -135,7 +135,7 @@ def rotate_search(pid, event_folder, output_folder, station_info_file, append_te
 
 	grid = np.zeros((NX + 1, NX + 1))
 
-	for station in station_list:
+	for station in rotation_coeff:
 		print("processing", station)
 		station_coord = (station_info[station]["lon"], station_info[station]["lat"])
 		#print(station_coord)
@@ -149,7 +149,7 @@ def rotate_search(pid, event_folder, output_folder, station_info_file, append_te
 
 				grid[i][j] += normS(_baz, *rotation_coeff[station])
 
-	grid /= len(station_list)
+	grid /= len(list(rotation_coeff.keys()))
 
 	# plt.contourf(grid.T, origin = "lower")
 	# plt.colorbar()