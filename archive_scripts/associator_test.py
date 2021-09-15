569e5a93fc39bc29b807673835cdb2c7332d1ba8
Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-09-15 11:13:52 +0800

    Ignore files

--- archive_scripts/associator_test.py
+++
@@ -1,5 +0,0 @@
-# use eqt associator as per https://eqtransformer.readthedocs.io/en/latest/tutorial.html#phase-association
-
-# but uhh i think it needs multiple stations so.... 
-
-# i guess i can run on like 5 stations for 5 days so it's more manageable on local machine
--- gmt/aceh/insettest.sh
+++
@@ -1,8 +0,0 @@
-gmt begin inset pdf
-  gmt basemap -R0/40/20/60 -JM6.5i -Bafg -B+glightgreen
-  gmt inset begin -DjTR+w2.5i+o0.2i -F+gpink+p0.5p -M0.25i
-    gmt basemap -Rg -JA20/20/2i -Bafg
-    gmt text -F+f18p+cTR+tINSET -Dj-0.15i -N
-  gmt inset end
-  gmt text -F+f18p+cBL+tMAP -Dj0.2i
-gmt end
