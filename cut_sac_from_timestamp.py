1a619f12bf156daec1a5015a3d11047d48f169be
Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-12-08 21:42:06 +0800

    Timezone removal

--- cut_sac_from_timestamp.py
+++ cut_sac_from_timestamp.py
@@ -142,7 +142,7 @@ def choose_event_wf(real_csv, real_json, input_csv, output_csv, output_json, sac
 
 	if write:
 
-		eqt_df["sac_start_time"] = pd.to_datetime(eqt_df["sac_start_time"])
+		eqt_df["sac_start_time"] = pd.to_datetime(eqt_df["sac_start_time"]).dt.tz_localize(None)
 
 		for index, row in real_df.iterrows():
 
