6a5785a667dbea1c65d1b3df0aa383616a8d13b4
Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-12-13 17:25:08 +0800

    Test real_wrapper on server

--- real_wrapper.py
+++ real_wrapper.py
@@ -17,7 +17,6 @@ def generate_job():
 	default_params = {
 		"lat_centre": 4.75,
 		# -R
-		"R_args": {
 		"gridsearch_horizontal_range_deg": 1 ,
 		"gridsearch_vertical_range_km": 60,
 		"gridsearch_horizontal_cellsize_deg" : 0.05,
@@ -29,18 +28,16 @@ def generate_job():
 		"station-event_greatcircle_dist_deg": 2,
 		# by default, the station location recording the initiating phase is used 
 		"gridsearch_centre_lat": None, 
-		"gridsearch_centre_lon": None, },
+		"gridsearch_centre_lon": None,
 
 		# -G optional, not needed if homogenous model is assumed
-		"G_args":{ 
 		"traveltime_horizontal_range_deg": 2.4, 
 		"traveltime_vertical_range_km": 35,
 		"traveltime_horizontal_cellsize_deg": 0.02,
-		"traveltime_vertical_cellsize_km": 1,},
+		"traveltime_vertical_cellsize_km": 1,
 		
 		# -V
 
-		"V_args" : {
 		"average_p_velocity_kms": 6.2,
 		"average_s_velocity_kms": 3.3,
 
@@ -48,10 +45,9 @@ def generate_job():
 		"consider_station_elevation": 0, 
 		"shallow_p_velocity_kms": None,
 		"shallow_s_velocity_kms": None,
-		},
+	
 
 		# -S
-		"S_args" : {
 		"n_p_picks": 3,
 		"n_s_picks": 3,
 		"n_total_picks": 6, # e.g. you could specify 0/0/4 for any combination of P and S
@@ -78,7 +74,7 @@ def generate_job():
 		"std_tolerance_factor": 4,
 		# used for synthetic resolution analysis
 		"ires": 0,
-		},
+		
 
 	}
 
@@ -90,7 +86,7 @@ def generate_job():
 		"tt_table_path": "/home/zchoong001/cy1400/cy1400-eqt/detections/febmar21/blank/REAL/tt_db/ttdb.txt",
 		"pick_dir_path": "/home/zchoong001/cy1400/cy1400-eqt/REAL/7jul_redojan/Pick/20200102" ,
 		"test_bench_folder": "/home/zchoong001/cy1400/cy1400-eqt/REAL/testbench",
-		"day_list_path ": "",
+		"day_list_path ": "/home/zchoong001/cy1400/cy1400-eqt/REAL/7jul_redojan/REAL/test_filelist.txt",
 	}
 
 	
@@ -123,15 +119,18 @@ def generate_job():
 	job_info_list = []
 
 	for v in itertools.product(*[vary_params[k] for k in vary_params]):
+		print(v)
+
+		change_key_list = list(vary_params.keys())
+
 		_job_dict = {}
-		for c, k in enumerate(vary_params):
-			_job_dict[k] = c
+		for c, k in enumerate(change_key_list):
+			_job_dict[k] = v[c]
 
 		job_info_list.append(_job_dict)
 
 	# save the job info dictionaries as json files later
 
-	print(job_info_list)
 
 	for expt_info in job_info_list:
 		_params = default_params
@@ -139,6 +138,7 @@ def generate_job():
 		for k in expt_info:
 			_params[k] = expt_info[k]
 
+		print(expt_info)
 		_date_info = {
 			"year":"2020",
 			"month":"01",
@@ -147,7 +147,11 @@ def generate_job():
 		}
 
 
-		print(call_REAL(_params, paths, _date_info))
+		test_fn_call = (call_REAL(_params, paths, _date_info))
+
+		output = check_output(test_fn_call.split(" "))
+
+		print(output)
 
 
 	if not os.path.exists(paths["test_bench_folder"]):
@@ -171,8 +175,27 @@ def generate_job():
 
 def generate_ymd(day_list_path):
 
+	file_list = []
+
 	with open(day_list_path, "r") as f:
-		pass
+		for line in f:
+			file_list.append(line.strip())
+
+	file_dict_list = []
+
+	for day in file_list:
+		_dict = {
+			"date_str": day,
+			"year": day[0:4],
+			"month": day[4:6],
+			"day": day[-2:],
+		}
+
+		file_dict_list.append(_dict)
+
+	return file_dict_list
+
+	
 
 def pbs_writer():
 	pass
@@ -237,24 +260,24 @@ def call_REAL(params, paths, date_info):
 		if (k == "gridsearch_centre_lon" or k == "gridsearch_centre_lat") and not params["R_args"]["gridsearch_centre_lon"]:
 			break
 		else:
-			call_string += "{:2f}/".format(params["R_args"][k])
+			call_string += "{:.2f}/".format(params["R_args"][k])
 
 		
 	call_string += " -G"
 	for k in params["G_args"]:
-		call_string += "{:2f}/".format(params["G_args"][k])
+		call_string += "{:.2f}/".format(params["G_args"][k])
 	
 	call_string += " -V"
 	for k in params["V_args"]:
 		if not params["V_args"]["consider_station_elevation"] and k == "consider_station_elevation":
 			break
 		else:
-			call_string += "{:2f}/".format(params["V_args"][k])
+			call_string += "{:.2f}/".format(params["V_args"][k])
 
 	call_string += " -S{:n}/{:n}/{:n}/{:n}/{}/{}/{}/{}/{}/{}/{}".format(
 		params["S_args"]["n_p_picks"],
-		params["S_args"]["n_p_picks"],
-		params["S_args"]["n_p_picks"],
+		params["S_args"]["n_s_picks"],
+		params["S_args"]["n_total_picks"],
 		params["S_args"]["n_both_ps_picks"],
 		params["S_args"]["stdev_residual_threshold_seconds"],
 		params["S_args"]["p-s_minimum_separation_seconds"],
@@ -267,7 +290,7 @@ def call_REAL(params, paths, date_info):
 
 	call_string += " {} {} {}".format(
 		paths["station_file_path"], 
-		os.path.join(paths["pick_dir"], date_info["date_str"] ),
+		os.path.join(paths["pick_dir_path"], date_info["date_str"] ),
 		paths["tt_table_path"]
 		)
 
@@ -276,7 +299,7 @@ def call_REAL(params, paths, date_info):
 
 
 
-if __name__ == "__init__":
+if __name__ == "__main__":
 	generate_job()
 
 
