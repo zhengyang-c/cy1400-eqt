1bd98585edda5145063195e24e497bf517431e86
Zheng Yang <zchoong001@e.ntu.edu.sg>
2022-01-06 10:22:58 +0800

    Add rel_dir

--- batch_sac.py
+++ batch_sac.py
@@ -4,10 +4,13 @@ import os
 import time
 
 
-def main(input_folder, output_sh):
+
+def main(input_folder, rel_dir, output_sh,):
+	os.chdir(rel_dir)
+
 	output_str = ""
 
-	file_list = [str(p) for p in Path(input_folder).glob("*SAC")]
+	file_list = [str(p) for p in Path(input_folder).rglob("*SAC")]
 
 	for f in file_list:
 		output_str += "printf 'r {}\\ninterpolate delta 0.01\\nw over\\nq\\n' | sac \n".format(f)	
@@ -24,6 +27,7 @@ def main(input_folder, output_sh):
 if __name__ == "__main__":
 	ap = argparse.ArgumentParser()
 	ap.add_argument("input_folder")
+	ap.add_argument("rel_dir")
 	ap.add_argument("output_sh")
 	args = ap.parse_args()
 	main(args.input_folder, args.output_sh)
