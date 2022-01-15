cd24c46e4c8a9f8263e44505780aa1c543729267
Zheng Yang <zchoong001@e.ntu.edu.sg>
2022-01-15 15:49:56 +0800

    Print debug

--- cut_sac_from_timestamp.py
+++ cut_sac_from_timestamp.py
@@ -206,20 +206,8 @@ def choose_event_wf(real_csv, real_json, input_csv, output_csv, output_json, sac
 				f2 = os.path.join(event_folder, event_id + ".EHN.SAC")
 				f3 = os.path.join(event_folder, event_id + ".EHZ.SAC")
 
-				start_time = (event_dt - _row.sac_start_time).total_seconds() - 30
-				end_time = (event_dt - _row.sac_start_time).total_seconds() + 120
-				print(start_time)
-				print(event_dt)
-				print(_row)
-				print(_row.sac_start_time)
-
-				raise ValueError
-
-				if not start_time:
-					print(start_time)
-					print(event_dt)
-					print(_row)
-					raise ValueError
+				start_time = (event_dt - _row.sac_start_dt).total_seconds() - 30
+				end_time = (event_dt - _row.sac_start_dt).total_seconds() + 120
 
 				cut_str += "printf \"cut {:.2f} {:.2f}\\nr {}\\nwrite SAC {} {} {}\\nq\\n\" | sac\n".format(start_time, end_time, sac_source, f1, f2, f3)
 
