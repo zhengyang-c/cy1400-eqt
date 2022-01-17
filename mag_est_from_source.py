d55f842ac08caff565ffbfa972f9385966925c83
Zheng Yang <zchoong001@e.ntu.edu.sg>
2022-01-17 22:54:27 +0800

    Rerun with try except

--- mag_est_from_source.py
+++ mag_est_from_source.py
@@ -91,7 +91,9 @@ def main():
 
 	own_pz = {'zeros': [0j, 0j, 0j], 'poles': [-2.199000e+01 +2.243000e+01j, -2.199000e+01 -2.243000e+01j], 'gain':1.029447e+09 , 'sensitivity': 1} 
 
-	df = df[df["source_file"] == "/home/eos_data/SEISMIC_DATA_TECTONICS/RAW/ACEH/MSEED/Deployed-2020-01-MSEED/A54/AC.A54.00.EH*.D.2020.030.000000.SAC"]
+
+	_f = "real_postprocessing/rereal/error.log"
+
 
 	for source_file, _df in df.groupby("source_file"):
 		print(source_file)
@@ -100,40 +102,43 @@ def main():
 		p_before = 0.5
 		print(_df)
 		for index, row in _df.iterrows():
-			stt = st.copy()
+			try:
+				stt = st.copy()
 
-			stt.trim(obspy.UTCDateTime(row.p_arrival_time + datetime.timedelta(seconds = -5)), obspy.UTCDateTime(row.s_arrival_time + datetime.timedelta(seconds = 4)))
+				stt.trim(obspy.UTCDateTime(row.p_arrival_time + datetime.timedelta(seconds = -5)), obspy.UTCDateTime(row.s_arrival_time + datetime.timedelta(seconds = 4)))
 
-			stt.detrend("demean")
-			stt.detrend("linear")
+				stt.detrend("demean")
+				stt.detrend("linear")
 
-			stt.simulate(paz_remove = own_pz, paz_simulate = paz_wa)
-			print(stt[0])
-			stt[0].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
-			stt[1].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
+				stt.simulate(paz_remove = own_pz, paz_simulate = paz_wa)
+				print(stt[0])
+				stt[0].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
+				stt[1].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
 
-			ch_e = stt[0].data
-			ch_n = stt[1].data
+				ch_e = stt[0].data
+				ch_n = stt[1].data
 
-			_id = str(int(row.ID)).zfill(6)
-			p_after = (row.s_arrival_time - row.p_arrival_time).total_seconds() + 3
+				_id = str(int(row.ID)).zfill(6)
+				p_after = (row.s_arrival_time - row.p_arrival_time).total_seconds() + 3
 
-			ptime_id = 500 #round((row.p_arrival_time - row.sac_start_dt).total_seconds()/delta)
-			start_id = ptime_id - round(p_before/delta)
-			end_id = ptime_id + round(p_after/delta)
+				ptime_id = 500 #round((row.p_arrival_time - row.sac_start_dt).total_seconds()/delta)
+				start_id = ptime_id - round(p_before/delta)
+				end_id = ptime_id + round(p_after/delta)
 
-			datatre = ch_e[start_id:end_id]
-			datatrn = ch_n[start_id:end_id]
+				datatre = ch_e[start_id:end_id]
+				datatrn = ch_n[start_id:end_id]
 
-			dist = station_dist[_id][row.station]
+				dist = station_dist[_id][row.station]
 
-			amp = (np.max(datatre) + np.abs(np.min(datatre)) + np.max(datatrn) + np.abs(np.min(datatrn)))/4 * 1000 * 15000 
-			# 15000 is for the nodes 
-			# 1000 is from meter to millimeter (mm) see Hutton and Boore (1987)
-			mag = math.log10(amp) + 1.110*math.log10(dist/100) + 0.00189*(dist-100) + 3.0
+				amp = (np.max(datatre) + np.abs(np.min(datatre)) + np.max(datatrn) + np.abs(np.min(datatrn)))/4 * 1000 * 15000 
+				# 15000 is for the nodes 
+				# 1000 is from meter to millimeter (mm) see Hutton and Boore (1987)
+				mag = math.log10(amp) + 1.110*math.log10(dist/100) + 0.00189*(dist-100) + 3.0
 
-			df.at[index, "mag"] = mag
+				df.at[index, "mag"] = mag
 
+			except:
+				_f.write("{} {}\n".format(source_file, index))
 	
 	df.to_csv(output_csv, index = False)
 
