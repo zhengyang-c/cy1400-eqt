bb206bf824b016fc86fc6e0549060d6aef054406
Zheng Yang <zchoong001@e.ntu.edu.sg>
2022-01-20 21:51:35 +0800

    Test with multi processing

--- mag_est_from_source.py
+++ mag_est_from_source.py
@@ -9,7 +9,8 @@ import json
 from utils  import parse_station_info
 import os
 import glob
-
+import multiprocessing as mp
+from itertools import repeat
 # need to know event distance 
 
 def dx(X1, X2):
@@ -76,6 +77,90 @@ def station_event_distances(station_file, event_csv, patched_csv, output_json):
 	with open(output_json, "w") as f:
 		json.dump(output_info, f, indent = 4)
 
+def worker(df_info, args):
+	source_file = df_info[0]	
+	_df = df_info[1]
+
+	station_dist = args["station_dist"]
+
+	paz_wa = {'sensitivity': 2080, 'zeros': [0j,0j], 'gain': 1, 'poles': [-5.4978 - 5.6089j, -5.4978 + 5.6089j]}
+
+	own_pz = {'zeros': [0j, 0j, 0j], 'poles': [-2.199000e+01 +2.243000e+01j, -2.199000e+01 -2.243000e+01j], 'gain':1.029447e+09 , 'sensitivity': 1} 
+	print(source_file)
+
+	output_indices = []
+	output_mags = []
+
+	try:
+
+		if len(glob.glob(source_file)):
+			st = obspy.read(source_file)
+			delta = st[0].stats.delta
+			p_before = 0.5
+		else:
+			# load remap
+
+			station_remap = {"A02": "A54", "A10": "TG03", "GE01": "GN01", "GE16": "GN16", "GE10": "GN10", "GE07":"GN07", "AS09":"AN09", "GE13":"GN13", "TA02":"TN02", "MA01": "MN01", "GE15":"GN16", "GM05":"GM55"}
+
+			rev_map = {v:k for k,v in station_remap.items()}
+
+			# get station from sourcefile, try changing station. if it fails, then raise Exception 
+
+			for k in rev_map:
+				if k in source_file:
+					source_file = source_file.replace(k, rev_map[k])
+					break
+			if not len(glob.glob(source_file)):
+				print("new source file", source_file)
+				raise ValueError
+
+			st = obspy.read(source_file)
+			delta = st[0].stats.delta
+			p_before = 0.5
+
+	except:
+		print("ERROR")
+	for index, row in _df.iterrows():
+		try:
+			stt = st.copy()
+
+			stt.trim(obspy.UTCDateTime(row.p_arrival_time + datetime.timedelta(seconds = -5)), obspy.UTCDateTime(row.s_arrival_time + datetime.timedelta(seconds = 4)))
+
+			stt.detrend("demean")
+			stt.detrend("linear")
+
+			stt.simulate(paz_remove = own_pz, paz_simulate = paz_wa)
+			stt[0].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
+			stt[1].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
+
+			ch_e = stt[0].data
+			ch_n = stt[1].data
+
+			_id = str(int(row.ID)).zfill(6)
+			p_after = (row.s_arrival_time - row.p_arrival_time).total_seconds() + 3
+
+			ptime_id = round(5/delta) #round((row.p_arrival_time - row.sac_start_dt).total_seconds()/delta)
+			start_id = ptime_id - round(p_before/delta)
+			end_id = ptime_id + round(p_after/delta)
+
+			datatre = ch_e[start_id:end_id]
+			datatrn = ch_n[start_id:end_id]
+
+			dist = station_dist[_id][row.station]
+
+			amp = (np.max(datatre) + np.abs(np.min(datatre)) + np.max(datatrn) + np.abs(np.min(datatrn)))/4 * 1000 * 15000 
+			# 15000 is for the nodes 
+			# 1000 is from meter to millimeter (mm) see Hutton and Boore (1987)
+			mag = math.log10(amp) + 1.110*math.log10(dist/100) + 0.00189*(dist-100) + 3.0
+
+			output_indices.append(index)
+			output_mags.append(mag)
+
+		except:
+			pass
+
+	return (output_indices, output_mags)
+
 def main(station_file, patched_csv, dist_json, output_csv, om = "", oe = ""):
 
 	# patched_csv = "real_postprocessing/rereal/patch_merged_eqt_rereal.csv"
@@ -102,100 +187,29 @@ def main(station_file, patched_csv, dist_json, output_csv, om = "", oe = ""):
 
 	# first go through the json to calculate station - event locations, of which there are a lot
 
-	paz_wa = {'sensitivity': 2080, 'zeros': [0j,0j], 'gain': 1, 'poles': [-5.4978 - 5.6089j, -5.4978 + 5.6089j]}
-
-	own_pz = {'zeros': [0j, 0j, 0j], 'poles': [-2.199000e+01 +2.243000e+01j, -2.199000e+01 -2.243000e+01j], 'gain':1.029447e+09 , 'sensitivity': 1} 
 	# stt is the stream data
 
 	if om:
 		_g = open(om, "w")
 	if oe:
 		_f = open(oe, "w")
-
-
-	for source_file, _df in df.groupby("source_file"):
-		print(source_file)
-		try:
-
-			if len(glob.glob(source_file)):
-				st = obspy.read(source_file)
-				delta = st[0].stats.delta
-				p_before = 0.5
-			else:
-				# load remap
-
-				station_remap = {"A02": "A54", "A10": "TG03", "GE01": "GN01", "GE16": "GN16", "GE10": "GN10", "GE07":"GN07", "AS09":"AN09", "GE13":"GN13", "TA02":"TN02", "MA01": "MN01", "GE15":"GN16", "GM05":"GM55"}
-
-				rev_map = {v:k for k,v in station_remap.items()}
-
-				# get station from sourcefile, try changing station. if it fails, then raise Exception 
-
-				for k in rev_map:
-					if k in source_file:
-						source_file = source_file.replace(k, rev_map[k])
-						break
-				if not len(glob.glob(source_file)):
-					print("new source file", source_file)
-					raise ValueError
-
-				st = obspy.read(source_file)
-				delta = st[0].stats.delta
-				p_before = 0.5
-
-		except:
-			print("ERROR")
-			# try using the station remapping, if not exit gracefully
-			if oe:
-				_g.write("error {}".format(source_file))
-			continue
-		for index, row in _df.iterrows():
-			try:
-				stt = st.copy()
-
-				stt.trim(obspy.UTCDateTime(row.p_arrival_time + datetime.timedelta(seconds = -5)), obspy.UTCDateTime(row.s_arrival_time + datetime.timedelta(seconds = 4)))
-
-				stt.detrend("demean")
-				stt.detrend("linear")
-
-				stt.simulate(paz_remove = own_pz, paz_simulate = paz_wa)
-				stt[0].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
-				stt[1].filter(type = "bandpass", freqmin = 0.2, freqmax = 20.0, zerophase = True)
-
-				ch_e = stt[0].data
-				ch_n = stt[1].data
-
-				_id = str(int(row.ID)).zfill(6)
-				p_after = (row.s_arrival_time - row.p_arrival_time).total_seconds() + 3
-
-				ptime_id = round(5/delta) #round((row.p_arrival_time - row.sac_start_dt).total_seconds()/delta)
-				start_id = ptime_id - round(p_before/delta)
-				end_id = ptime_id + round(p_after/delta)
-
-				datatre = ch_e[start_id:end_id]
-				datatrn = ch_n[start_id:end_id]
-
-				dist = station_dist[_id][row.station]
-
-				amp = (np.max(datatre) + np.abs(np.min(datatre)) + np.max(datatrn) + np.abs(np.min(datatrn)))/4 * 1000 * 15000 
-				# 15000 is for the nodes 
-				# 1000 is from meter to millimeter (mm) see Hutton and Boore (1987)
-				mag = math.log10(amp) + 1.110*math.log10(dist/100) + 0.00189*(dist-100) + 3.0
-
-				df.at[index, "mag"] = mag
-
-				if om:
-					_g.write("{} {:.4f}\n".format(index, mag))
-
-			except:
-				if oe:
-					_f.write("{} {}\n".format(source_file, index))
-				print("{} {}".format(source_file, index))
 	
-	df.to_csv(output_csv, index = False)
-	if oe:
-		_f.close()
-	if om:
-		_g.close()
+
+	with mp.Pool(mp.cpu_count_()) as p:
+		groupby = df.groupby("source_file")
+		groups = [(group, groupby.get_group(group)) for group in groupby.groups] 
+
+		indices, mags = zip(*p.map(worker, zip(groups, repeat({"station_dist": station_dist}))))
+	
+	for i in range(len(indices)):
+		for j in indices[i]:
+			df.at[j, "m_l"] = mags[i][j]
+	
+	df.to_csv(output_csv)
+
+
+
+
 
 
 if __name__ == "__main__":
