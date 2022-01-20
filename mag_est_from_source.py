0f52ee300af30555283baeec5dc5ca5702c674d9
Zheng Yang <zchoong001@e.ntu.edu.sg>
2022-01-20 22:09:40 +0800

    Fix

--- mag_est_from_source.py
+++ mag_est_from_source.py
@@ -196,7 +196,7 @@ def main(station_file, patched_csv, dist_json, output_csv, om = "", oe = ""):
 		print(indices, mags)
 	
 	for i in range(len(indices)):
-		for j in range(len(indices[i]):
+		for j in range(len(indices[i])):
 			df.at[indices[i][j], "m_l"] = mags[i][j]
 	
 	df.to_csv(output_csv)
